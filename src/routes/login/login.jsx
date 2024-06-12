32ad7729a7ba311cf637edf6f9195fd0d1a9cdb4
Rajeev Yadav <rajeev.396yadav@gmail.com>
2024-06-12 20:55:33 +0545

    initialize,login/register,userprofile,createpost,searchpost

---
+++ src/routes/login/login.jsx
@@ -0,0 +1,79 @@
+import { useContext, useState } from 'react';
+import './login.scss';
+import { Link, useNavigate } from 'react-router-dom';
+import apiRequest from '../../lib/apiRequest';
+import { AuthContext } from '../../context/AuthContext';
+
+function Login() {
+  const [error, setError] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+  const navigate = useNavigate();
+
+  
+  const { updateUser } = useContext(AuthContext);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setError("")
+    setIsLoading(true);
+    const formData = new FormData(e.target);
+
+    const username = formData.get('username');
+    // const email = formData.get('email');
+    const password = formData.get('password');
+
+    // console.log(username, email, password);
+    try {
+      const res = await apiRequest.post(
+        '/auth/login',
+        {
+          username,
+          password,
+        }
+      );
+
+      updateUser(res.data)
+
+      // console.log(res)
+      navigate('/');
+      // console.log(res.data);
+    } catch (err) {
+      setError(err.response.data.message);
+      // console.log(err);
+    }finally{
+      setIsLoading(false)
+    }
+  };
+
+  return (
+    <div className="login">
+      <div className="formContainer">
+        <form onSubmit={handleSubmit}>
+          <h1>Welcome back</h1>
+          <input
+            name="username"
+            required
+            minLength={3}
+            maxLength={20}
+            type="text"
+            placeholder="Username"
+          />
+          <input
+            name="password"
+            type="password"
+            required
+            placeholder="Password"
+          />
+          <button disabled={isLoading}>Login</button>
+          {error && <span>{error}</span>}
+          <Link to="/register">{"Don't"} you have an account?</Link>
+        </form>
+      </div>
+      <div className="imgContainer">
+        <img src="/bg.png" alt="" />
+      </div>
+    </div>
+  );
+}
+
+export default Login;
